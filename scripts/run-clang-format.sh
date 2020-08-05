77e67823292aa4553b982e7703b80154d84252a0
Jakob Maljaars <jakob.maljaars@stcorp.nl>
2020-08-05 09:52:05 +0200

    Update formatting script

--- scripts/run-clang-format.sh
+++ scripts/run-clang-format.sh
@@ -12,25 +12,32 @@
 SCRIPT_PATH=$(dirname "$0")
 cd $SCRIPT_PATH
 
-#SCRIPT CONFIGURATION FOR THIS REPO. Adjust it when copying to a different repo.
+#Script configuration for this repo. Adjust it when copying to a different repo.
 
-#The directory that contains the source files, which clang-format should format. 
+#The directory containing the source files, which clang-format should format.
 #Should be relative to $SCRIPT_PATH
 SOURCE_DIR=..
 
-#Directories that must be excluded from formatting. These paths are
-#relative to SCRIPT_PATH.
-EXCLUDE_DIRS=()
+#Directories that must be excluded from formatting. These paths are relative to
+#$SCRIPT_PATH (e.g. ../external) or should match a pattern (e.g. */external).
+EXCLUDE_DIRS=(../external)
 
 #The extensions of the source files, which clang-format should format.
 SOURCE_EXT=(*.cpp *.h)
 
-#END SCRIPT CONFIGURATION.
+#End script configuration.
 
 set -e
 
+# Detect run environment.
+if [ -n "$CI" ]; then
+  DRYRUN=" (dry run on CI)"
+elif [ -n "$GIT_AUTHOR_DATE" ]; then
+  DRYRUN=" (dry run in git hook)"
+fi
+
 # print in bold-face
-echo -e "\e[1mRunning clang-format...\e[0m"
+echo -e "\e[1mRunning clang-format$DRYRUN...\e[0m"
 
 # Convert SOURCE_EXT into "-name ext1 -o -name ext2 -o name ext3 ..."
 FIND_NAMES="-name ${SOURCE_EXT[0]}"
@@ -44,15 +51,23 @@ for e in ${EXCLUDE_DIRS[*]}; do
   FIND_EXCLUDES+="-path $e -prune -o "
 done
 
-find $SOURCE_DIR $FIND_EXCLUDES -type f \( $FIND_NAMES \) \
-  -exec clang-format -i -style=file \{\} +
+FILES=$(find $SOURCE_DIR $FIND_EXCLUDES -type f \( $FIND_NAMES \) -print)
 
-if git diff --exit-code --quiet; then
-    # print in bold-face green
-    echo -e "\e[1m\e[32mGreat job, git shows no changed files!\e[0m"
-    exit 0;
-else
+if [ -n "$DRYRUN" ]; then
+  # If the xml has no replacement entries, all files are formatted.
+  if clang-format -style=file --output-replacements-xml $FILES |
+     grep -q "<replacement "; then
     # Print in bold-face red
-    echo -e "\e[1m\e[31mGit shows at least one changed file now!\e[0m"
+    echo -e "\e[1m\e[31mAt least one file is not properly formatted!\e[0m"
+    echo -e "\e[1m\e[31mRun $0 for formatting all files!\e[0m"
     exit 1;
+  else
+    # print in bold-face green
+    echo -e "\e[1m\e[32mGreat job, all files are properly formatted!\e[0m"
+    exit 0;
+  fi
+else
+  clang-format -i -style=file $FILES
+  # print in bold-face
+  echo -e "\e[1mSuccessfully formatted all files.\e[0m"
 fi
