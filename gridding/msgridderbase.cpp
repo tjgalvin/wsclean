54a6217478bb20f25528da714ea328c6cdd1b4c5
Jakob Maljaars <jakobmaljaars@gmail.com>
2021-09-17 14:10:36 +0000

    Support scalar gain corrections

--- gridding/msgridderbase.cpp
+++ gridding/msgridderbase.cpp
@@ -573,25 +573,24 @@ void MSGridderBase::initializeMeasurementSet(MSGridderBase::MSData& msData,
             &_h5parm->GetSolTab(_settings.facetSolutionTables[idxP]));
       }
 
-      if (!_h5SolTabs.first->HasAxis("pol") ||
-          !_h5SolTabs.second->HasAxis("pol")) {
-        throw std::runtime_error(
-            "One of the solution tables does not contain a 'pol' "
-            "(polarization) axis");
+      const size_t npol1 = _h5SolTabs.first->HasAxis("pol")
+                               ? _h5SolTabs.first->GetAxis("pol").size
+                               : 1;
+      const size_t npol2 = _h5SolTabs.second->HasAxis("pol")
+                               ? _h5SolTabs.second->GetAxis("pol").size
+                               : 1;
+      if (npol1 == 1 && npol2 == 1) {
+        _correctType = JonesParameters::CorrectType::SCALARGAIN;
+      } else if (npol1 == 2 && npol2 == 2) {
+        _correctType = JonesParameters::CorrectType::GAIN;
+      } else if (npol1 == 4 && npol2 == 4) {
+        _correctType = JonesParameters::CorrectType::FULLJONES;
       } else {
-        const size_t npol1 = _h5SolTabs.first->GetAxis("pol").size;
-        const size_t npol2 = _h5SolTabs.second->GetAxis("pol").size;
-        if (npol1 == 2 && npol2 == 2) {
-          _correctType = JonesParameters::CorrectType::GAIN;
-        } else if (npol1 == 4 && npol2 == 4) {
-          _correctType = JonesParameters::CorrectType::FULLJONES;
-        } else {
-          throw std::runtime_error(
-              "Incorrect or mismatching number of polarizations in the "
-              "provided soltabs. Number of polarizations should be either all "
-              "2 or all 4, but received " +
-              std::to_string(npol1) + " and " + std::to_string(npol2));
-        }
+        throw std::runtime_error(
+            "Incorrect or mismatching number of polarizations in the "
+            "provided soltabs. Number of polarizations should be either "
+            "all 1, 2 or 4, but received " +
+            std::to_string(npol1) + " and " + std::to_string(npol2));
       }
     } else {
       throw std::runtime_error(
