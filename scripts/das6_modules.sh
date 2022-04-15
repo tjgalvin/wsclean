b92e07b14d390e6f57eab8102313332dff8b0c6a
Jakob Maljaars <jakobmaljaars@gmail.com>
2022-04-15 08:37:40 +0000

    Build with ninja and fix pipeline

--- scripts/das6_modules.sh
+++ scripts/das6_modules.sh
@@ -8,7 +8,7 @@ module load casacore/3.4.0
 module load cfitsio/3.49
 module load fftw/3.3.10
 module load hdf5/1.10.7
-module load openblas/0.3.18-mt
+module load openblas/0.3.18
 module load openmpi/4.1.2
 
 module load python/3.9.9
@@ -16,5 +16,5 @@ module load py-astropy/4.0.1.post1
 module load py-numpy/1.22.0
 module load py-pytest/6.2.5
 
-module load everybeam/latest
+module load everybeam/0.3.1
 module load idg/latest
